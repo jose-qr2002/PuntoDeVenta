02546e98d0cd56fe96b65bee8bd88d5bf9a75e54
Jose Quispe <jkqr2016@gmail.com>
2024-03-27 20:49:58 -0500

    Haciendo responsive la vista de Productos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -8,20 +8,18 @@
 
 @section('contenidoprincipal')
     <h1 class="text-center mt-5">PRODUCTOS</h1>
-    <div class="controls d-flex justify-content-between">
-        <a href="">
-            <button class="btn btn-primary">AGREGAR PRODUCTO</button>
+    <div class="controls d-flex flex-column gap-4 flex-md-row align-items-md-center justify-content-md-between">
+        <a href="" class="btn btn-primary d-block">
+            AGREGAR PRODUCTO
         </a>
-        <div class="input-group w-25">
+        <div class="d-flex flex-column flex-lg-row align-items-lg-center gap-2">
             <input type="text" class="form-control" placeholder="Buscar Producto">
-            <div class="input-group-append ms-2">
-                <button class="btn btn-outline-secondary" type="button">Buscar</button>
-            </div>
+            <button class="btn btn-outline-secondary w-100" type="button">Buscar</button>
         </div>
     </div>
     <div class="table-responsive">
-        <table class="table mt-3 table-striped table-bordered">
-            <thead>
+        <table class="table mt-3 table-striped table-bordered text-center" style="min-width: 700px">
+            <thead class="table-dark">
                 <tr>
                     <th>Nro</th>
                     <th>Nombre</th>
@@ -32,19 +30,116 @@
                 </tr>
             </thead>
             <tbody>
-                @for ($i = 0; $i < 10; $i++)
-                    <tr>
-                        <td>{{$i+1}}</td>
-                        <td>Llave Inglesa</td>
-                        <td>30</td>
-                        <td>3</td>
-                        <td>unidades</td>
-                        <td>
-                            <button class="btn btn-secondary">Editar</button>
-                            <button class="btn btn-danger">Eliminar</button>
-                        </td>
-                    </tr>
-                @endfor
+                <tr>
+                    <td>1</td>
+                    <td>Llave Inglesa</td>
+                    <td>30</td>
+                    <td>3</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Taladro eléctrico</td>
+                    <td>50</td>
+                    <td>S/30</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Caja de herramientas</td>
+                    <td>40</td>
+                    <td>S/29</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Sierra eléctrica</td>
+                    <td>30</td>
+                    <td>S/79</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Cinta métrica</td>
+                    <td>120</td>
+                    <td>S/7</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Martillo de carpintero</td>
+                    <td>100</td>
+                    <td>S/12</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Llave ajustable</td>
+                    <td>60</td>
+                    <td>S/14</td>
+                    <td>Pieza</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>Cinta adhesiva de doble cara</td>
+                    <td>50</td>
+                    <td>S/5</td>
+                    <td>Rollo</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>9</td>
+                    <td>Guantes de trabajo</td>
+                    <td>75</td>
+                    <td>S/10</td>
+                    <td>Par</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>10</td>
+                    <td>Pintura blanca</td>
+                    <td>20</td>
+                    <td>S/25</td>
+                    <td>Galón</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
