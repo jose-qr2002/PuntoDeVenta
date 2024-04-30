aa19264a8ed7f82e583cfbcd094838c2898d4218
Jose  Quispe <jkqr2016@gmail.com>
2024-04-30 16:02:02 -0500

    Agregando archivo ProductoTest

---
+++ tests/Feature/ProductoTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_producto_index(): void 
+    {
+
+    }
+
+    public function test_producto_create(): void
+    {
+        
+    }
+
+    public function test_producto_create_validation(): void
+    {
+
+    }
+
+    public function test_producto_create_exception(): void
+    {
+
+    }
+
+    public function test_producto_update():void
+    {
+
+    }
+
+    public function test_producto_update_validation():void 
+    {
+
+    }
+
+    public function test_producto_update_exception():void
+    {
+
+    }
+
+    public function test_producto_delete():void
+    {
+
+    }
+
+    public function test_producto_delete_exception():void
+    {
+
+    }
+}
