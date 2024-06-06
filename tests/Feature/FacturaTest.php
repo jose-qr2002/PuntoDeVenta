7cd0e4fb7df34bc22520df3fb1c0cc1a49067c54
Luis Laqui <luissenati20@gmail.com>
2024-06-06 14:17:27 -0500

    PUN-18 Creamos testFactura create y validation

--- tests/Feature/FacturaTest.php
+++ tests/Feature/FacturaTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Cliente;
+use App\Models\Factura;
 
 class FacturaTest extends TestCase
 {
@@ -16,4 +18,43 @@ class FacturaTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_factura_create_success(): void
+{
+    $clienteId = 10;
+
+    $facturaData = [
+        'cliente_id' => $clienteId,
+        'fecha' => now()->toDateString(),
+        'estado' => 'pendiente',
+        'metodopago_id' => 1,
+        'monto_total' => 0,
+    ];
+
+    $response = $this->post(route('factura.store'), $facturaData);
+    $response->assertStatus(302);
+
+   $this->assertDatabaseHas('facturas',[
+        'cliente_id' => $clienteId,
+        'fecha' => now()->toDateString(),
+        'estado' => 'pendiente',
+        'metodopago_id' => 1,
+        'monto_total' => 0,
+        ]);
+    
+    $response->assertRedirect(route('registrar.venta'));
+}
+
+public function test_factura_create_validation(): void
+{
+    $facturaData = [
+        'fecha' => now()->toDateString(),
+        'estado' => 'pendiente',
+        'metodopago_id' => 1,
+        'monto_total' => 0,
+    ];
+    $response = $this->post(route('factura.store'), $facturaData);
+    $response->assertStatus(302);
+    $response->assertSessionHasErrors(['cliente_id']);
+}
 }
