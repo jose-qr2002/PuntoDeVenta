d52e0c3d992d22e9257b948ef0f47b8c07b5a65b
Luis Laqui <luissenati20@gmail.com>
2024-06-06 18:38:33 -0500

    PUN-22 Cargar Facturas en la vista Ventas

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -12,7 +12,8 @@ class FacturaController extends Controller
 {
     public function index()
     {
-        return view('ventas');
+        $facturas = Factura::all();
+        return view('ventas', compact('facturas'));
     }
 
     public function generarVenta(Request $request)
