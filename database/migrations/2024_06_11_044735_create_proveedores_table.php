fc9dc631c745049eac03c97b6658f53fb118f8d7
Jose  Quispe <jkqr2016@gmail.com>
2024-06-11 00:00:11 -0500

    PUN-25 creando migracion para crear tabla proveedores

---
+++ database/migrations/2024_06_11_044735_create_proveedores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('ruc')->unique();
+            $table->string('razon_social');
+            $table->string('telefono');
+            $table->string('correo');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('provincia');
+            $table->string('codigo_postal');
+            $table->enum('estado', ['activo', 'inactivo'])->default('activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proveedores');
+    }
+};
