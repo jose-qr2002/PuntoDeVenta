07f697cc0160b3a4fb740c342b5fde1a505bf009
Jose  Quispe <jkqr2016@gmail.com>
2024-06-13 22:07:06 -0500

    PUN-34 Agregando metodo de busqueda para ventas

--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -14,12 +14,12 @@
         <div class="col-12 col-lg-6 mb-3 mb-lg-0">
             <div class="row">
                 <div class="col-12 col-lg-6 mb-3 mb-lg-0">
-                    <a href="" class="btn btn-outline-secondary w-100">
+                    <a href="" class="btn btn-outline-secondary w-100" data-bs-toggle="modal" data-bs-target="#fechaModal">
                         Buscar por Fecha
                     </a>
                 </div>
                 <div class="col-12 col-lg-6">
-                    <a href="" class="btn btn-outline-secondary w-100">
+                    <a href="" class="btn btn-outline-secondary w-100" data-bs-toggle="modal" data-bs-target="#clienteModal">
                         Buscar por cliente
                     </a>
                 </div>
@@ -39,7 +39,7 @@
     <table class="table table-striped table-bordered text-center mb-0" style="min-width: 700px">
         <thead class="table-dark">
             <tr>
-                <th>Nro</th>
+                <th>ID</th>
                 <th>Cliente</th>
                 <th>Fecha</th>
                 <th>Estado de pago</th>
@@ -70,7 +70,52 @@
     <div class="animate__animated animate__fadeInRight">
         {{ $facturas->links() }}
     </div>
+  
+    <!-- Modal Fecha -->
+    <div class="modal fade" id="fechaModal" tabindex="-1" aria-labelledby="fechaModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="fechaModalLabel">BUSCAR POR FECHA</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('ventas') }}" method="GET">
+                        <input type="hidden" name="metodoBusqueda" value="fecha">
+                        <input type="date" name="parametro" class="form-control mb-2">
+                        <div class="row">
+                            <div class="col-12 col-sm-4 offset-sm-4">
+                                <button type="submit" class="btn btn-primary w-100">Buscar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- Modal Cliente -->
+    <div class="modal fade" id="clienteModal" tabindex="-1" aria-labelledby="clienteModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="clienteModalLabel">BUSCAR POR CLIENTE</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('ventas') }}" method="GET">
+                        <input type="hidden" name="metodoBusqueda" value="cliente">
+                        <input type="text" name="parametro" class="form-control mb-2" placeholder="DNI del Cliente">
+                        <div class="row">
+                            <div class="col-12 col-sm-4 offset-sm-4">
+                                <button type="submit" class="btn btn-primary w-100">Buscar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @if(session('msn_success'))
 
 <script>
